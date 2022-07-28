c4154038952d01dc97aac9e2abb2096d6a52d206
jhivanb <jhivanb@gmail.com>
2022-07-28 18:03:22 +0530

    added logs to identity check

--- handler.go
+++ handler.go
@@ -1,16 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
 
-var data  map[string]int = map[string]int {
-	"jhivan":23,
-	"jhiva":24,
-	"jhiv":25,
-	"jhi":26,
-	"jh":27,
-
+var data map[string]int = map[string]int{
+	"jhivan": 23,
+	"jhiva":  24,
+	"jhiv":   25,
+	"jhi":    26,
+	"jh":     27,
 }
+
+type userData struct {
+	UID int    `json:"uid"`
+	ID  string `json:"ID"`
+}
+
 func IdentityCheck(c *gin.Context) {
-	c.request.
+	user := userData{}
+
+	err := c.BindJSON(&user)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, "Couldn't parse json request")
+	}
+
+	log.Print(user)
 }
--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	authGroup := router.Group("/").Use(AuthMiddleware())
+	authGroup := router.Group("/")
+	// .Use(AuthMiddleware())
 
 	authGroup.POST("/", IdentityCheck)
 
